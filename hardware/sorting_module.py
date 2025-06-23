7126e27ba41ae99f61be9971f141b8e3511ac43f
jamesxu <xxj8711@gmail.com>
2025-06-23 10:09:21 +0800

    sorting_mudle update

--- hardware/sorting_module.py
+++ hardware/sorting_module.py
@@ -1,20 +1,29 @@
 from common.serial_device import SerialDevice
 from common.logger import logger
-from common.command_loader import load_commands, build_command
+from common.command_loader import build_command
 
 class SortingModule(SerialDevice):
+    CACHE = {
+        "left": "854B0000290F006D",       # Turn left
+        "middle": "854B0000200F0064",     # Back to middle
+        "right": "854B0000090F004D"       # Turn right
+    }
+
     def __init__(self, port, baudrate):
         super().__init__(port, baudrate)
-        self.commands = load_commands()["hardware_1_sorting_module"]
+        self._comm_enabled = False
+
+    def send_command(self, key: str):
+        if key not in self.CACHE:
+            raise ValueError(f"Invalid sorting command '{key}'. Valid options: {list(self.CACHE.keys())}")
+        logger.info(f"Sorter: Executing '{key}' command")
+        self.send(build_command(self.CACHE[key], add_crc=False))
 
     def turn_left(self):
-        logger.info("Sorter: Turning left")
-        self.send(build_command(self.commands["turn_left"], add_crc=False))
+        self.send_command("left")
 
     def back_to_middle(self):
-        logger.info("Sorter: Returning to middle")
-        self.send(build_command(self.commands["back_to_middle"], add_crc=False))
+        self.send_command("middle")
 
     def turn_right(self):
-        logger.info("Sorter: Turning right")
-        self.send(build_command(self.commands["turn_right"], add_crc=False))
+        self.send_command("right")
